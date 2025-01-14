73cebd8d2ae2ba68424e4d927cb769e1ac56e542
Piotrek <piotrmachowski93@gmail.com>
2025-01-14 20:33:31 +0100

    First commit with initial logic

---
+++ menu-api/menu/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+# menu/models.py
+
+from django.db import models
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
